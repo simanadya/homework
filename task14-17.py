796f48debcaa43c72091cdb1598c20cd133b9038
simanadya <Simanovichnadezhda@gmail.com>
2017-05-06 21:00:42 +0300

    Update task14-17.py
    15 и 17 не работает и я не вижу ошибки
    16 - не понимаю , почему неправильно

--- task14-17.py
+++ task14-17.py
@@ -4,69 +4,69 @@ digit = int(input("Введите число:"))
 
 def is_even(x):
     return x%2 == 0
-if digit%2==0:
-    return True
+if is_even(digit):
+    print("%d is even" % digit)
 else:
+    print("%d is odd" % digit)
    
-
-
-
-
 # # Task 15===========================================
+
+import math
+x1 = int(input("Координaта x1:"))
+y1 = int(input("Координaта y1:"))
+r1 = int(input("Координата радиуса r1:"))
+x2 = int(input("Координaта x2:"))
+y2 = int(input("Координaта y2:"))
+r2 = int(input("Координата радиуса r2:"))
+result = math.sqrt((x2 - x1)**2 + (y2 - y1)**2 + (r2 - r1)**2)
+
+
+def intersection_of_the_circles(x1, y1, r1, x2, y2, r2):
+    return x1, y1, r1, x2, y2, r2 == 0
+if intersection_of_the_circles(result):
+    print ("%result - Intersectioned" % result)
+else:
+    print("%result - Not Intersectioned" % result)
+    
+
+# Task 16===================================================
+
+v1 = 12
+v2 = 70
+time1 = 4/v1
+time2 = 6/v2
+
+def train_clash (v1, v2):
+    return v1, v2 ==0
+if time1<time2:
+    print ("clash")
+else:
+    print("ok")
+
 #
-# x1 = int(input("Координaта x1:"))
-# y1 = int(input("Координaта y1:"))
-# r1 = int(input("Координата радиуса r1:"))
-# x2 = int(input("Координaта x2:"))
-# y2 = int(input("Координaта y2:"))
-# r2 = int(input("Координата радиуса r2:"))
-#
-# import math
-# def intersection_of_the_circles(x1, y1, r1, x2, y2, r2):
-#     return x1, y1, r1, x2, y2, r2 ==0
-# if math.sqrt((x2 - x1)**2 + (y2 - y1)**2 + (r2 - r1)**2) == 0:
-#     print ("Intersectioned")
-# else:
-#     print("Not intersectioned")
-#
-#
-# # Task 16===================================================
-#
-# v1 = 12
-# v2 = 70
-# time1 = 4/v1
-# time2 = 6/v2
-#
-# def train_clash (v1, v2):
-#     return v1, v2 ==0
-# if time1<time2:
-#     print ("clash")
-# else:
-#     print("ok")
-#
-# #
-# # Task 17========================================
-#
-# print("Введите коэффициенты для квадратного уравнения (ax^2 + bx + c = 0):")
-# a = int(input("a = "))
-# b = int(input("b = "))
-# c = int(input("c = "))
-#
-#
-# Discriminant = int(b**2) - int(4) * int(a) * int(c)
-# print("D = ", Discriminant)
-#
-# def solve_equation (a, b, c ):
-#     if Discriminant > 0:
-#         D = (int(Discriminant) ** 0.5)
-#         x1 = (-b + Discriminant) / (2 * a)
-#         print("x1 = ", x1)
-#         x2 = (-b - Discriminant) / (2 * a)
-#         print("x2 = ", x2)
-#     elif Discriminant == 0:
-#         D = -b / (2 * a)
-#         print("x = ", Discriminant)
-#     else:
-#         print("None")
-#
-# print("Equation (a=%d, b=%d, c=%d) has roots: %s, %s" % (a, b, c, x1, x2))
+# Task 17========================================
+
+print("Введите коэффициенты для квадратного уравнения (ax^2 + bx + c = 0):")
+a = int(input("a = "))
+b = int(input("b = "))
+c = int(input("c = "))
+
+
+Discriminant = int(b**2) - int(4) * int(a) * int(c)
+print("D = ", Discriminant)
+
+def solve_equation (a, b, c ):
+    if Discriminant > 0:
+        D = (int(Discriminant) ** 0.5)
+        x1 = (-b + Discriminant) / (2 * a)
+        print("x1 = ", x1)
+        x2 = (-b - Discriminant) / (2 * a)
+        print("x2 = ", x2)
+    elif Discriminant == 0:
+        D = -b / (2 * a)
+        print("x = ", Discriminant)
+    else:
+        print("None")
+
+x1, x2 = solve_equation(a, b, c)
+print("Equation (a=%d, b=%d, c=%d) has roots: %s, %s" % (a, b, c, x1, x2))
