6b1646fb4b987eae199e952abe17923f38ccdc6e
Nadiya Simanovich <nadia@gmail.com>
2017-05-16 22:09:01 +0300

    я не знаю как сделать 6е

--- control_work.py
+++ control_work.py
@@ -25,58 +25,69 @@ for x in n:
         mult *= int(x)
 print("Произведение нечётных чисел = ", mult)
 
-
-
 #Task 5=============================================
 
+import math
 d1 = int(input("Введите первое число: "))
 d2 = int(input("Введите второе число: "))
 my_position = 10
-distance1 = int(my_position - d1)
-distance2 = int(my_position - d2)
+distance1 = math.fabs(int(my_position - d1))
+distance2 = math.fabs(int(my_position - d2))
 
-# def diff_dist(d1, d2):
-for d1, d2 in range (distance1, distance1):
+def diff_dist(d1, d2):
     if distance2 == distance1:
-        print ("Числа ", d1," и ", d2," равноудалены от ", my_position)
+        print("Числа ", d1, " и ", d2," равноудалены от ", my_position)
     if distance1 > distance2:
-        print ("Число ", d2, " ближе к ", my_position)
+        print("Число ", d2, " ближе к ", my_position)
     if distance1 < distance2:
-        print ("Число ", d1, " ближе к ", my_position)
-    # return diff_dist(distance1, distance2)
-# diff_dist(dist1).sort(key = my_position)
-# abs.sort(key = diff_dist)
-# print(diff_dist(d1, d2))
-print()
-# #Task 7============================================
-#
-# fib_list = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
-#
-# def listsum(fib_list):
-#     sum = 0
-#     for i in fib_list:
-#         sum = sum + i
-#     return sum
-#
-# print("Сумма десяти первых чисел ряда Фибоначчи = ", listsum([0, 1, 1, 2, 3, 5, 8, 13, 21, 34]))
+        print("Число ", d1, " ближе к ", my_position)
+    return diff_dist
+print(diff_dist(d1, d2))
 
 #Task 6=================================================
-#
-# from random import random
+
+lst = [2,5,4,3,6,7,9,0,5,8]
+
+import random
+
+
 # N = 10
 # a = [0,0,0,0,0,0,0,0,0,0]
 # b = [0,0,0,0,0,0,0,0,0,0]
 # c = [0,0,0,0,0,0,0,0,0,0]
-# for i in range(N):
-#     a[i] = int(random() * 100)
+def random_list(lst):
+    # for i in range(len(list)):
+        lst = []
+        import random
+        # for elem in enumerate(lst):
+        list = [randint(lst) for i in range(n)]
+        # lst = random.shuffle(lst)
+        return random_list
 # print("Введите числа: ")
-# for i in range(N):
+# for i in range(len(list)):
 #     b[i] = int(input())
-# for i in range(N):
+# for i in range(len(list)):
 #     c[i] = a[i] + b[i]
-# print(a)
+print(lst)
 # print(b)
 # print(c)
+
+#Task 7============================================
+
+def fib_list(n):
+    d = [0, 1]
+    for i in range(2, n + 1):
+        d.append(d[i-1] + d[i-2])
+    print(d)
+n = 10
+fib_list(n)
+
+def listsum(fib_list):
+    sum = 0
+    for i in fib_list:
+        sum = sum + i
+    return sum
+print("Сумма десяти первых чисел ряда Фибоначчи = ", sum)
 
 #Task 8========================================
 
